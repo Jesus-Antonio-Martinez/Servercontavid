09949f97464d5df3fd9e61dade060276722333e7
Daniel Limas <amedlimas97@gmail.com>
2020-11-21 16:43:44 -0700

    Ready to be deployed on heroku

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-app.set('port',process.env.PORT || 80)
+// Express server to serv vue js build files
+
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 4000;
+app.listen(port);
