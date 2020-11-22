23bb3690b6dfc83514590db6d90793fd816b34c7
Jesus-Antonio-Martinez <43158805+Jesus-Antonio-Martinez@users.noreply.github.com>
2020-11-21 17:58:11 -0700

    Segunda Rama

---
+++ router/index.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router"
+import Home from "@/src/components/HelloWorld";
+import Menu from "@/src/components/Menu.vue";
+
+const routes = [
+ {
+     path: "/",
+     name: "Home",
+     component: HelloWorld,
+ },
+ {
+     path: "/Menu",
+     name: "Menu",
+     component: Menu,
+ },
+];
+
+const router = createRouter([
+    history: createWebHistory(),
+    routes,
+]);
+
+export default router;
